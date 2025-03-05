247b8e1bd417f063d4de88566f323810ace22edd
VMTechy <vicmr1994@gmail.com>
2025-03-05 23:10:57 +0100

    testing with my supabase project

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://wmzhlpusxveejjscjmab.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtemhscHVzeHZlZWpqc2NqbWFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MzMyOTQsImV4cCI6MjA1NjAwOTI5NH0.v8QFbcF0sqUH3PO8Dgw3MbFuy6m6xhriqDXLliT7iVA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
